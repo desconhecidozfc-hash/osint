cfd51738fbf10468f3bff4f04f77fa93cb9a6fb6
desconhecidozfc-hash <desconhecidozfc@gmail.com>
2025-12-07 15:01:49 -0300

    Deploy via MonoBuild - 2025-12-07T18:01:48.379Z

---
+++ script.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: {
+                            900: '#000000',
+                            100: '#ffffff'
+                        }
+                    }
+                }
+            }
+        }
